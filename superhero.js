754c24cd289ff5d04b80181720e252999480a09a
siansrd <sianrobinsondavies@gmail.com>
2018-09-28 15:40:38 +0100

    Modify ending

--- superhero.js
+++ superhero.js
@@ -1,3 +1,3 @@
 // A day in the life of a superhero
 
-// Once upon a time a superhero woke up. She was tired of fixing all the mess made by the humans. So she stayed at home, watched TV and ate ice cream.
+// Once upon a time a superhero woke up. She was tired of fixing all the mess made by the humans. So she stayed at home, watched TV and ate popcorn.
